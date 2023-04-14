a126d9ea799c1edefb25f3dfd12d04c905f34998
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-04-14 19:14:42 +0200

    Aggiornamento readme

--- Snack-3/assets/js/main.js
+++ Snack-3/assets/js/main.js
@@ -1,8 +1,10 @@
 /*
 CONSEGNA: Snack-3
-Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
-se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
-La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
-Usiamo i nuovi metodi degli array foreach o filter.
+    • Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+
+    • L'array generato da parte vostra dovrà essere di nomi o cose, ad esempio: ['alberto', 'michele','angelo','simone'].
+
+    • La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri (se come numeri,ad esempio, ho 2 e 4, devo inserire in un array nuovo l'elemento in posizione 3).
+    
+    • Usiamo i nuovi metodi degli array foreach o filter.
 */
